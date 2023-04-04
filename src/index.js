dc80d23f472f7950d6a392a6a9010a28f0744b8e
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-04-04 01:49:33 -0300

    REQ 5

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs').promises;
 const generateToken = require('./utils/generateToken');
-const readData = require('./utils/fsUtils');
+//const readData = require('./utils/fsUtils');
 const validateEmail = require('./middlewares/validateEmail');
 const validadePassword = require('./middlewares/validatePassword');
+const validateRate = require('./middlewares/validateRate');
+const validateWatchedAt = require('./middlewares/validateWatchedAt');
+const validateTalk = require('./middlewares/validateTalk');
+const validateAuth = require('./middlewares/validateAuth');
+const validateName = require('./middlewares/validateName');
+const validateAge = require('./middlewares/validateAge');
 
 const talkerPath = path.resolve(__dirname, 'talker.json');
 
-const app = express();
+
+const readData = async (pathUrl) => {
+  const obj = await fs.readFile(pathUrl);
+  return JSON.parse(obj);
+};
+
+const writeFile = async (pathUrl, toWrite) => {
+  await fs.writeFile(pathUrl, JSON.stringify(toWrite));
+};
+
+  const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
@@ -42,6 +59,18 @@ app.post('/login', validateEmail, validadePassword, (req, res) => {
   } catch (err) { 
     res.status(500).send({ message: err.message });
   }
+});
+
+app.post('/talker', 
+  validateAuth, validateName, validateAge, validateTalk, 
+  validateRate, validateWatchedAt, async (req, res) => {
+  const obj = await readData(talkerPath);
+  const talker = req.body;
+  talker.id = obj.length + 1;
+  obj.push(talker);
+  await writeFile(talkerPath, obj);
+
+  res.status(201).json(talker);
 });
 
 app.listen(PORT, () => {
---
+++ src/middlewares/validateRate.js
@@ -0,0 +1,10 @@
+module.exports = (req, res, next) => {
+    const { rate } = req.body.talk;
+     if (!Number.isInteger(rate) || rate > 5 || rate < 1) {
+       return res.status(400).json(
+         { message: 'O campo \"rate\" deve ser um número inteiro entre 1 e 5' },
+       );
+     }
+  
+    next();
+  };
